edf1c174d6d4235eaafefbb5532202b1fbd0f95d
Alisson1207 <alisson.viteri01@epn.edu.ec>
2025-06-03 20:29:49 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { supabase } from '../supabase.client';
+
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [IonicModule, CommonModule],
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
 })
 export class HomePage {
+  selectedFile: File | null = null;
+  uploadStatus = '';
+  uploadedUrl: string | null = null;
 
-  constructor() {}
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
 
+  async uploadFile() {
+    if (!this.selectedFile) {
+      this.uploadStatus = 'Selecciona un archivo primero.';
+      return;
+    }
+
+    this.uploadStatus = 'Subiendo archivo...';
+
+    try {
+      const { data, error } = await supabase.storage
+        .from('archivos') // 
+        .upload(`archivos/${this.selectedFile.name}`, this.selectedFile, {
+          cacheControl: '3600',
+          upsert: false,
+        });
+
+      if (error) throw error;
+
+      const { data: publicUrl } = supabase.storage
+        .from('nombre-del-bucket')
+        .getPublicUrl(`archivos/${this.selectedFile.name}`);
+
+      this.uploadedUrl = publicUrl.publicUrl;
+      this.uploadStatus = 'Archivo subido correctamente.';
+    } catch (error: any) {
+      this.uploadStatus = 'Error al subir: ' + error.message;
+    }
+  }
 }
